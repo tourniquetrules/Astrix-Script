32eea3dd42753f358dae93823cb55b5669d82341
tourniquetrules <tourniquetrules@gmail.com>
2024-09-07 13:44:46 -0500

    Update astrix.sh

--- astrix.sh
+++ astrix.sh
@@ -9,9 +9,9 @@ lld lldb llvm-dev llvm-runtime \
 llvm python3-clang -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sudo apt install cargo -y;
-exec $SHELL
 cargo install wasm-pack
 ./.cargo/bin/rustup target add wasm32-unknown-unknown
 git clone https://github.com/astrix-network/astrix-node
 cd astrix-node
+exec $SHELL
 cargo run --release --bin astrixd -- --utxoindex
