7ab6889958718b8b8f841528ada9e092b8791217
tourniquetrules <tourniquetrules@gmail.com>
2024-09-07 10:13:49 -0500

    Create astrixminer.sh

---
+++ astrixminer.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/astrix-network/astrix-miner
+cd astrix-miner
+cargo build --release
+cd target/release
+./astrix-miner --mining-address astrix:qqjzemurre653qvkxehfk4x2ngvdnzfusnyr85y8hu04gvq4jcv8q53frrp42
